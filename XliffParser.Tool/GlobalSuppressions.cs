68aaed57c19bad1522181fae99718fab70f73c99
Florian Mücke <fmuecke@gmail.com>
2016-07-13 22:21:32 +0200

    updated nuspec

--- XliffParser.Tool/GlobalSuppressions.cs
+++ XliffParser.Tool/GlobalSuppressions.cs
@@ -3,5 +3,6 @@
 // Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
 
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1101:Prefix local calls with this", Justification = "no this pointer pollution")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1652:Enable XML documentation output", Justification = "no xml documentation output desired")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:File must have header", Justification = "file headers not needed")]
--- create_nupkg.cmd
+++ create_nupkg.cmd
@@ -1,1 +1,2 @@
 nuget pack XLiffParser\XliffParser.csproj -Properties Configuration=Release
+nuget pack XLiffParser.Tool\XliffParser.Tool.csproj -Properties Configuration=Release
