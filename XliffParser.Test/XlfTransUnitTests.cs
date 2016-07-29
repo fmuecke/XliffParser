d9bd7e3e8f6497be349057dd7ebb366a1d3991a9
Florian Mücke <fmuecke@gmail.com>
2016-07-29 23:05:34 +0200

    removed some unnecessary strings from test data

--- XliffParser.Test/XlfTransUnitTests.cs
+++ XliffParser.Test/XlfTransUnitTests.cs
@@ -15,113 +15,43 @@
         private static string xlf11doc =
             "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
             "<xliff version=\"1.1\" xmlns:rwt=\"http://www.schaudin.com/xmlns/rwt11\">" +
-            "  <file original=\"Strings.resx\" source-language=\"en\" datatype=\"resx\" tool=\"RC-WinTrans\" rwt:crc=\"0\" target-language=\"en-US\">" +
+            "  <file original=\"Strings.resx\" source-language=\"en\" datatype=\"resx\" tool=\"RC-WinTrans\" target-language=\"en-US\">" +
             "    <header>" +
             "      <rwt:crc>0</rwt:crc>" +
             "    </header>" +
             "    <body>" +
-            "      <group id=\"Strings.resx\" restype=\"x-stringtable\" rwt:crc=\"0\">" +
-            "        <trans-unit resname=\"RememberLogonInformation\" id=\"none\" restype=\"x-text\" rwt:crc=\"6abc3d6d\">" +
+            "      <group id=\"Strings.resx\" restype=\"x-stringtable\">" +
+            "        <trans-unit resname=\"RememberLogonInformation\" id=\"none\" restype=\"x-text\">" +
             "          <source>_Save logon information</source>" +
             "          <target state=\"translated\">_Save logon information</target>" +
-            "          <rwt:historylist>" +
-            "            <rwt:historyevent>" +
-            "              <rwt:origin>IMPORT_RESFILE</rwt:origin>" +
-            "              <rwt:date>2015-11-23T15:24:11Z</rwt:date>" +
-            "              <rwt:user>fmuecke</rwt:user>" +
-            "              <rwt:description>Strings.en.resx</rwt:description>" +
-            "            </rwt:historyevent>" +
-            "          </rwt:historylist>" +
             "        </trans-unit>" +
-            "        <trans-unit resname=\"BMC_Title\" id=\"none\" restype=\"x-text\" rwt:crc=\"99f5b604\">" +
+            "        <trans-unit resname=\"BMC_Title\" id=\"none\" restype=\"x-text\">" +
             "          <source>muitool Management Center</source>" +
             "          <target state=\"final\">muitool Management Center</target>" +
-            "          <rwt:historylist>" +
-            "            <rwt:historyevent>" +
-            "              <rwt:origin>IMPORT_RESFILE</rwt:origin>" +
-            "              <rwt:date>2015-11-23T15:24:14Z</rwt:date>" +
-            "              <rwt:user>fmuecke</rwt:user>" +
-            "              <rwt:description>Strings.en.resx</rwt:description>" +
-            "            </rwt:historyevent>" +
-            "            <rwt:historyevent>" +
-            "              <rwt:origin>TRANSLBAR</rwt:origin>" +
-            "              <rwt:date>2016-02-04T12:13:05Z</rwt:date>" +
-            "              <rwt:user>fhinze</rwt:user>" +
-            "              <rwt:target>muitool Management Center</rwt:target>" +
-            "            </rwt:historyevent>" +
-            "          </rwt:historylist>" +
             "        </trans-unit>" +
-            "        <trans-unit resname=\"Button_AssignJobs\" id=\"none\" restype=\"x-text\" rwt:crc=\"450e08e9\">" +
+            "        <trans-unit resname=\"Button_AssignJobs\" id=\"none\" restype=\"x-text\">" +
             "          <source>Assign Jobs</source>" +
             "          <target state=\"translated\">Assign jobs</target>" +
-            "          <rwt:historylist>" +
-            "            <rwt:historyevent>" +
-            "              <rwt:origin>IMPORT_RESFILE</rwt:origin>" +
-            "              <rwt:date>2015-11-23T15:24:14Z</rwt:date>" +
-            "              <rwt:user>fmuecke</rwt:user>" +
-            "              <rwt:description>Strings.en.resx</rwt:description>" +
-            "            </rwt:historyevent>" +
-            "          </rwt:historylist>" +
             "        </trans-unit>" +
-            "        <trans-unit resname=\"Button_RevokeLicense\" id=\"none\" restype=\"x-text\" rwt:crc=\"b7bd98ab\">" +
+            "        <trans-unit resname=\"Button_RevokeLicense\" id=\"none\" restype=\"x-text\">" +
             "          <source>Revoke license</source>" +
             "          <target state=\"translated\">Revoke license</target>" +
-            "          <rwt:historylist>" +
-            "            <rwt:historyevent>" +
-            "              <rwt:origin>IMPORT_RESFILE</rwt:origin>" +
-            "              <rwt:date>2015-11-23T15:24:12Z</rwt:date>" +
-            "              <rwt:user>fmuecke</rwt:user>" +
-            "              <rwt:description>Strings.en.resx</rwt:description>" +
-            "            </rwt:historyevent>" +
-            "          </rwt:historylist>" +
             "        </trans-unit>" +
-            "        <trans-unit resname=\"Button_Cancel\" id=\"none\" restype=\"x-text\" rwt:crc=\"af96ad97\">" +
+            "        <trans-unit resname=\"Button_Cancel\" id=\"none\" restype=\"x-text\">" +
             "          <source>Cancel</source>" +
             "          <target state=\"final\">Cancel</target>" +
-            "          <rwt:historylist>" +
-            "            <rwt:historyevent>" +
-            "              <rwt:origin>IMPORT_RESFILE</rwt:origin>" +
-            "              <rwt:date>2015-11-23T15:24:12Z</rwt:date>" +
-            "              <rwt:user>fmuecke</rwt:user>" +
-            "              <rwt:description>Strings.en.resx</rwt:description>" +
-            "            </rwt:historyevent>" +
-            "          </rwt:historylist>" +
             "        </trans-unit>" +
-            "        <trans-unit resname=\"Button_CloseApplication\" id=\"none\" restype=\"x-text\" rwt:crc=\"53a20b53\">" +
+            "        <trans-unit resname=\"Button_CloseApplication\" id=\"none\" restype=\"x-text\">" +
             "          <source>Close Application</source>" +
             "          <target state=\"translated\">Close application</target>" +
-            "          <rwt:historylist>" +
-            "            <rwt:historyevent>" +
-            "              <rwt:origin>IMPORT_RESFILE</rwt:origin>" +
-            "              <rwt:date>2015-11-23T15:24:11Z</rwt:date>" +
-            "              <rwt:user>fmuecke</rwt:user>" +
-            "              <rwt:description>Strings.en.resx</rwt:description>" +
-            "            </rwt:historyevent>" +
-            "          </rwt:historylist>" +
             "        </trans-unit>" +
-            "        <trans-unit resname=\"Button_Connect\" id=\"none\" restype=\"x-text\" rwt:crc=\"574ae704\">" +
+            "        <trans-unit resname=\"Button_Connect\" id=\"none\" restype=\"x-text\">" +
             "          <source>_Connect</source>" +
             "          <target state=\"final\">_Connect</target>" +
-            "          <rwt:historylist>" +
-            "            <rwt:historyevent>" +
-            "              <rwt:origin>IMPORT_RESFILE</rwt:origin>" +
-            "              <rwt:date>2015-11-23T15:24:11Z</rwt:date>" +
-            "              <rwt:user>fmuecke</rwt:user>" +
-            "              <rwt:description>Strings.en.resx</rwt:description>" +
-            "            </rwt:historyevent>" +
-            "          </rwt:historylist>" +
             "        </trans-unit>" +
-            "        <trans-unit resname=\"Button_Continue\" id=\"none\" restype=\"x-text\" rwt:crc=\"ebe7ebba\">" +
+            "        <trans-unit resname=\"Button_Continue\" id=\"none\" restype=\"x-text\">" +
             "          <source>Continue</source>" +
             "          <target state=\"final\">Continue</target>" +
-            "          <rwt:historylist>" +
-            "            <rwt:historyevent>" +
-            "              <rwt:origin>IMPORT_RESFILE</rwt:origin>" +
-            "              <rwt:date>2015-11-23T15:24:10Z</rwt:date>" +
-            "              <rwt:user>fmuecke</rwt:user>" +
-            "              <rwt:description>Strings.en.resx</rwt:description>" +
-            "            </rwt:historyevent>" +
-            "          </rwt:historylist>" +
             "        </trans-unit>" +
             "	  </group>" +
             "    </body>" +
@@ -184,7 +114,7 @@
             ns = doc.Root.Name.Namespace;
             unit = new XlfTransUnit(doc.Descendants(ns + "trans-unit").First(), ns);
             unit.Optional.AddNote("Valar morghulis!", "XliffParserTest");
-            isWithNote = doc.ToString().Replace(" ", string.Empty).Contains("</rwt:historylist>\r\n<notefrom=\"XliffParserTest\">Valarmorghulis!</note>\r\n</trans-unit>\r\n<trans-unit");
+            isWithNote = doc.ToString().Replace(" ", string.Empty).Contains("</target>\r\n<notefrom=\"XliffParserTest\">Valarmorghulis!</note>\r\n</trans-unit>\r\n<trans-unit");
             Assert.IsTrue(isWithNote);
         }
 
